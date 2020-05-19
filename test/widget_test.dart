2ec5ec0f4974695037c96cf860be225930328252
Hubert Salwin <salwinwwy@gmail.com>
2020-05-19 17:54:01 +0000

    feat: token storage, default request header

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,34 +1,10 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:PickApp/main.dart';
-import 'package:PickApp/authentication/authentication_bloc.dart';
-import 'package:PickApp/authentication/authentication_event.dart';
-import 'package:PickApp/repositories/userRepository.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    final userRepository = UserRepository();
-    // Build our app and trigger a frame.
-     await tester.pumpWidget(
-      BlocProvider<AuthenticationBloc>(
-        create: (context) {
-          return AuthenticationBloc(userRepository: userRepository)..add(AppStarted());
-        },
-        child: MyApp(userRepository: userRepository)
-      ),
-    );
-
     await tester.pumpAndSettle(Duration(seconds: 10));
 
-    await tester.tap(find.widgetWithText(FlatButton, 'Login'));
     await tester.pump();
   });
 }
