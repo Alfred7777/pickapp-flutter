c09becf8da4494a3b7982fe771a017cab5c888b1
Szymon Kuleczka <szymonkuleczka98@gmail.com>
2020-06-02 23:54:59 +0200

    chore: extract map business logic to bloc

---
+++ lib/map/map_event.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+class MapEvent extends Equatable {
+  const MapEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FetchLocations extends MapEvent {}
--- lib/repositories/eventRepository.dart
+++ lib/repositories/eventRepository.dart
@@ -18,36 +18,41 @@ class EventRepository {
     var lat = pos.latitude;
     var lon = pos.longitude;
     var client = AuthenticatedApiClient();
-    
-    var body = {'name': '$name', 'description': '$description', 'start_datetime_ms': startDatetime, 'end_datetime_ms': endDatetime, 'lat': lat, 'lon': lon, 'organiser_id': '$userID', 'discipline_id': '$disciplineID'};
+
+    var body = {
+      'name': '$name',
+      'description': '$description',
+      'start_datetime_ms': startDatetime,
+      'end_datetime_ms': endDatetime,
+      'lat': lat,
+      'lon': lon,
+      'organiser_id': '$userID',
+      'discipline_id': '$disciplineID'
+    };
 
     var response = await client.post('events', body: body);
     if (response.statusCode == 201) {
       return 'Event created';
-    }
-    else {
+    } else {
       throw Exception('Could not create event');
     }
   }
 
-  Future<Set<Marker>> getEvents() async {
-    Marker _mapEventToMarker(Map<String, dynamic> event) {
-      return Marker(
-        markerId: MarkerId(event['id']),
-        position: LatLng(event['lat'], event['lon'])
-      );
-    }
+  Future<Set<Location>> getMap() async {
     final client = AuthenticatedApiClient();
     final url = 'map';
     var response = await client.get(url);
 
     if (response.statusCode == 200) {
-      return json.decode(response.body).map<Marker>((event) => _mapEventToMarker(event)).toSet();
+      return json
+          .decode(response.body)
+          .map<Location>((locationJson) => Location.fromJson(locationJson))
+          .toSet();
     } else {
-      throw Exception('Failed to get event locations');
+      throw Exception('Failed to fetch map');
     }
   }
-  
+
   Future<List<Discipline>> getDisciplines() async {
     final client = AuthenticatedApiClient();
     final url = 'disciplines';
@@ -55,10 +60,30 @@ class EventRepository {
     var response = await client.get(url);
 
     if (response.statusCode == 200) {
-      return json.decode(response.body).map<Discipline>((discipline) => Discipline.fromJson(discipline)).toList();
+      return json
+          .decode(response.body)
+          .map<Discipline>((discipline) => Discipline.fromJson(discipline))
+          .toList();
     } else {
       throw Exception('Failed to get disciplines');
     }
+  }
+}
+
+class Location {
+  final String id;
+  final double lat;
+  final double lon;
+  final String disciplineId;
+
+  Location({this.id, this.lat, this.lon, this.disciplineId});
+
+  factory Location.fromJson(Map<String, dynamic> json) {
+    return Location(
+        id: json['id'],
+        lat: json['lat'],
+        lon: json['lon'],
+        disciplineId: json['discipline_id']);
   }
 }
 
