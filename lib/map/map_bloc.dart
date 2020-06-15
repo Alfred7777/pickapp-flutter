6748f3e8ca144255531f0592651e4c29e658eb44
Hubert Salwin <salwinwwy@gmail.com>
2020-06-15 14:45:56 +0000

    feat: markers with discipline icon

--- lib/map/map_bloc.dart
+++ lib/map/map_bloc.dart
@@ -1,4 +1,6 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:meta/meta.dart';
 import 'package:PickApp/map/map_event.dart';
 import 'package:PickApp/map/map_state.dart';
@@ -16,8 +18,17 @@ class MapBloc extends Bloc<MapEvent, MapState> {
   Stream<MapState> mapEventToState(MapEvent event) async* {
     if (event is FetchLocations) {
       var locations = await eventRepository.getMap();
+      var disciplines = await eventRepository.getDisciplines();
+      var icons = <String, BitmapDescriptor>{};
 
-      yield MapReady(locations: locations);
+      for (var discipline in disciplines) {
+        var icon = await BitmapDescriptor.fromAssetImage(
+            ImageConfiguration(size: Size(28, 28), devicePixelRatio: 2.5),
+            'assets/images/${discipline.id}.png');
+        icons.addAll({discipline.id: icon});
+      }
+
+      yield MapReady(locations: locations, icons: icons);
       return;
     }
   }
--- test/map/map_bloc_test.dart
+++ test/map/map_bloc_test.dart
@@ -8,13 +8,18 @@ import 'package:mockito/mockito.dart';
 
 import '../mocks.dart';
 
-void main() {
+void main() async{
+  TestWidgetsFlutterBinding.ensureInitialized();
   var _eventRepositoryMock = MockEventRepository();
   var _location =
       Location(id: 'id', lat: 100.50, lon: 100.50, disciplineID: 'discipline');
+  var _discipline = 
+      Discipline('b5c6e905-6b06-4c92-8c4f-5abc8dd44bfa', 'Basketball');
 
   when(_eventRepositoryMock.getMap())
       .thenAnswer((_) => Future.value({_location}));
+  when(_eventRepositoryMock.getDisciplines())
+      .thenAnswer((_) => Future.value([_discipline]));
 
   group('MapBloc', () {
     blocTest(
@@ -37,7 +42,8 @@ void main() {
       expect: [isA<MapReady>()],
       verify: (bloc) async {
         verify(_eventRepositoryMock.getMap()).called(1);
-        expect(bloc.state, equals(MapReady(locations: {_location})));
+        verify(_eventRepositoryMock.getDisciplines()).called(1);
+        expect(bloc.state, equals(MapReady(locations: {_location}, icons:{})));
       },
     );
   });
